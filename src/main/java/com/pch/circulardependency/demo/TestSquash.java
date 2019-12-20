d349f9b74164eba3f1c83f72057b1d5106a51365
Pavlo_Chechehov <pavlo.chechehov@refinitiv.com>
2019-12-20 09:55:28 +0200

    combine commits in 1

---
+++ src/main/java/com/pch/circulardependency/demo/TestSquash.java
@@ -0,0 +1,14 @@
+package com.pch.circulardependency.demo;
+
+public class TestSquash {
+
+  public static void main(String[] args) {
+    System.out.println("args = " + args);
+
+    int a = 2;
+
+    int b = 3;
+
+    int c = 4;
+  }
+}
